d53cc986472bab0d04f90458df49e00205bff730
acevallos <cevalloa@gmail.com>
2016-12-17 02:51:02 -0800

    - Updated detail aciton method to use action result types

--- src/ComicBookGgallery/ComicBookGgallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGgallery/ComicBookGgallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,16 @@ namespace ComicBookGgallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Index()
-        {
-            return View ();
-        }
+		public ActionResult Detail()
+		{
+			if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
+			{
+				return Redirect("/");
+			}
+
+
+				return Content("Hello from the comic book controller");
+
+		}
     }
 }
